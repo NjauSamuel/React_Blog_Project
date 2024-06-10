c05fa4658a3f874bbf52d8c64e7146f26977c974
NjauSamuel <njaus602@gmail.com>
2024-06-10 11:29:17 +0300

    Learnt about State in the markdown.txt

--- src/components/ClassBasedComponent.jsx
+++ src/components/ClassBasedComponent.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
 class ClassBasedComponent extends React.Component{
+    constructor(props){
+        console.log(props);
+        super(props);
+        this.state = {course: "react"}
+    }
     render(){
         return (
             <div>
                 <p>Class based Component</p>
                 <span>Props: {this.props.test}</span>
+
+                <span>Children: {this.props.children}</span>
+
+                <p>State: This is a {this.state.course} course. </p>
             </div>
         )
     }
