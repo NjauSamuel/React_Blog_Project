a2e00fabce941d555d00abdfb37093a9a57e430f
NjauSamuel <njaus602@gmail.com>
2024-06-10 17:48:12 +0300

    Learnt an event Handler introduction and also practiced my CSS skills alot.

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
 const FunctionalComponent = () => {
-    const [data, setData] = React.useState("react");
+    const [data, setData] = React.useState("react native");
+
+    const handleClick = () => {
+        console.log("This is onClick event!")
+        setData("vue")
+    }
 
     console.log(data)
 
     return(
         <div>
             <p className="py-4"><u>This is a functional component. </u></p>
+
+            <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>Click Me !</button>
         </div>
     )
 }
