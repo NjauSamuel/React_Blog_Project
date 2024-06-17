9e8c000e71d33b31cb1b1fcbc5bd18c1cb1902d1
NjauSamuel <njaus602@gmail.com>
2024-06-17 11:38:11 +0300

    I conditionally rendered a login and logout functionality and also learn how to use state to increment a counter and toggle.

--- src/components/ConditionalRendering.jsx
+++ src/components/ConditionalRendering.jsx
@@ -3,12 +3,22 @@ import {useState} from "react"
 const ConditionalRendering = () => {
 
     const [course, setCourse] = useState("vue")
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const handleLogin = () => { setIsLoggedIn(true) }
+    const handleLogout = () => { setIsLoggedIn(false) }
+
+    let text = null
+    isLoggedIn ? (text = "Welcome user!") : (text = "Click the login button to login!")
 
     return (
         <div>
             <p className="pt-3 pb-2 "><u>Conditional Rendering</u></p>
             {/* expression ? true-statement : false-statement */}
             {course === "react" ? (<p>This is react course</p>) : course === "vue" ? (<p>This is vue course</p>) : null}
+
+            <p className="pt-2">{text}</p>
+            {isLoggedIn === false ? <button className="bg-green-100 hover:bg-green-200 font-bold" onClick={handleLogin}>Login</button> : <button className="bg-red-500 hover:bg-red-700 text-white font-bold" onClick={handleLogout}>Logout</button>}
         </div>
     )
 }
--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 const FunctionalComponent = () => {
-    const [data, setData] = React.useState("react native");
+    const [data, setData] = React.useState("react");
+    
+    const [count, setCount] = React.useState(0);
 
     const handleClick = (course) => {
         console.log("This is onClick event!")
-        setData("angular")
+        setData(prevData => prevData === "react" ? "vue" : "react")
+        setCount(prevCount => prevCount + 1);
     }
 
     console.log(data)
@@ -15,6 +18,8 @@ const FunctionalComponent = () => {
             <p className="py-4"><u>This is a functional component. </u></p>
 
             <button type="button" onClick={() => handleClick("vue")} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Click Me !</button>
+
+            <p>Count: {count}</p>
 
             <p>State: {data}</p>
         </div>
