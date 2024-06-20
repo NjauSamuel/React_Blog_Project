9e1ebab96cfa46446fdd9046d3aea6831357bba3
NjauSamuel <njaus602@gmail.com>
2024-06-20 12:37:46 +0300

    Create a higher order component function.

---
+++ src/components/HOC/withCounter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+const withCounter = (Component) => {
+
+    const NewComponent = () => {
+
+        const [counter, setCounter] = useState(0)
+
+        const handleIncrement = () => {
+            setCounter(oldValue => oldValue + 1)
+        }
+
+        return (
+            <Component counter={counter} handleIncrement={handleIncrement} />
+        )
+    }
+
+    return NewComponent;
+
+}
+
+export default withCounter;
